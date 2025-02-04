65eafd44e609821dd9151a9bea6c71850d07a134
hmyge2k23 <hackeriua2k21@gmail.com>
2025-02-04 16:07:05 +0000

    suppression_du_champ_ref_projet)

--- models/sale_order.py
+++ models/sale_order.py
@@ -3,5 +3,5 @@ from odoo import models, fields
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    custom_field = fields.Char(string="Champ personnalisé", store=True)
-    project_reference = fields.Char(string="Référence projet", store=True)
+    custom_field = fields.Char(string="Titre du devis", store=True)
+    # project_reference = fields.Char(string="Référence projet", store=True) 
